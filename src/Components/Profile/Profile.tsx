38ffbbe1da373aad662d6abdf8dd8a23ab24489b
martynosa <martynosa@hotmail.com>
2023-06-25 21:13:40 +0300

    error fix

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -15,7 +15,7 @@ const Profile: React.FC<{ profile: IProfile }> = ({ profile }) => {
 
   return (
     <section className={classes['profile-container']}>
-      <Nav github={profile.github} linkedIn={profile.linkedIn} />
+      <Nav github={profile.gitHub} linkedIn={profile.linkedIn} />
       <ThemeNav />
       <div className={classes.details}>
         <div className={classes.summary}>
