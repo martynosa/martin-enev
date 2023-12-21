eb375c2db48ffdcdf2c84de5d870fb70f324e3e8
martiN <78382341+martynosa@users.noreply.github.com>
2023-12-21 22:22:24 +0200

    added tech stack tooltips

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,9 +1,9 @@
-import classes from './Profile.module.css';
-import { useTypewriter, Cursor } from 'react-simple-typewriter';
-import Nav from '../Common/Nav';
-import ThemeNav from '../Common/ThemeNav';
-import { useColors } from '../../ColorsContext';
-import { IProfile } from '../../util/interfaces';
+import classes from "./Profile.module.css";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import Nav from "../Common/Nav";
+import ThemeNav from "../Common/ThemeNav";
+import { useColors } from "../../ColorsContext";
+import { IProfile } from "../../util/interfaces";
 
 const Profile: React.FC<{ profile: IProfile }> = ({ profile }) => {
   const { color } = useColors();
@@ -14,7 +14,7 @@ const Profile: React.FC<{ profile: IProfile }> = ({ profile }) => {
   });
 
   return (
-    <section className={classes['profile-container']}>
+    <section className={classes["profile-container"]}>
       <Nav gitHub={profile.gitHub} linkedIn={profile.linkedIn} />
       <ThemeNav />
       <div className={classes.details}>
@@ -32,54 +32,54 @@ const Profile: React.FC<{ profile: IProfile }> = ({ profile }) => {
         <div className={classes.stack}>
           <h2 className={classes.question}>Tech stack?</h2>
           <div className={classes.tech}>
-            <img className={classes.icon} src="/tech-svg/html.svg" alt="html" />
-            <img className={classes.icon} src="/tech-svg/css.svg" alt="css" />
-            <img
-              className={classes.icon}
-              src="/tech-svg/javascript.svg"
-              alt="javascript"
-            />
-            <img
-              className={classes.icon}
-              src="/tech-svg/typescript.svg"
-              alt="typescript"
-            />
-            <img
-              className={classes.icon}
-              src="/tech-svg/nodejs.svg"
-              alt="nodejs"
-            />
-            <img
-              className={classes.icon}
-              src="/tech-svg/express.svg"
-              alt="express"
-            />
-            <img className={classes.icon} src="/tech-svg/git.svg" alt="git" />
-            <img
-              className={classes.icon}
-              src="/tech-svg/react.svg"
-              alt="react"
-            />
-            <img
-              className={classes.icon}
-              src="/tech-svg/redux.svg"
-              alt="redux"
-            />
-            <img
-              className={classes.icon}
-              src="./tech-svg/angular.svg"
-              alt="angular"
-            />
-            <img
-              className={classes.icon}
-              src="./tech-svg/firebase.svg"
-              alt="firebase"
-            />
-            <img
-              className={classes.icon}
-              src="./tech-svg/mongodb.svg"
-              alt="mongodb"
-            />
+            <div className={classes.icon}>
+              <img src="/tech-svg/html.svg" alt="html" />
+              <span className={classes.tooltip}>html</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="/tech-svg/css.svg" alt="css" />
+              <span className={classes.tooltip}>css</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="/tech-svg/javascript.svg" alt="javascript" />
+              <span className={classes.tooltip}>javascript</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="/tech-svg/typescript.svg" alt="typescript" />
+              <span className={classes.tooltip}>typescript</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="/tech-svg/nodejs.svg" alt="nodejs" />
+              <span className={classes.tooltip}>nodejs</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="/tech-svg/express.svg" alt="express" />
+              <span className={classes.tooltip}>express</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="/tech-svg/git.svg" alt="git" />
+              <span className={classes.tooltip}>git</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="/tech-svg/react.svg" alt="react" />
+              <span className={classes.tooltip}>react</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="/tech-svg/redux.svg" alt="redux" />
+              <span className={classes.tooltip}>redux</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="./tech-svg/angular.svg" alt="angular" />
+              <span className={classes.tooltip}>angular</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="./tech-svg/firebase.svg" alt="firebase" />
+              <span className={classes.tooltip}>firebase</span>
+            </div>
+            <div className={classes.icon}>
+              <img src="./tech-svg/mongodb.svg" alt="mongodb" />
+              <span className={classes.tooltip}>mongodb</span>
+            </div>
           </div>
         </div>
       </div>
